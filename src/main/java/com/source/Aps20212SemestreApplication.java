e6f57930ae9be1b532efda98899e00e26013a6e3
Eduardo Muterle <eduardomuterle@outlook.com>
2021-09-26 19:32:49 -0300

    Geração de arquivos do projeto

---
+++ src/main/java/com/source/Aps20212SemestreApplication.java
@@ -0,0 +1,13 @@
+package com.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aps20212SemestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aps20212SemestreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/source/Aps20212SemestreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.source;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aps20212SemestreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

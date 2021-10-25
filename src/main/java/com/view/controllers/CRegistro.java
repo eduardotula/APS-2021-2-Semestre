6b78e2d930db0eddd4b146e24bbfe727e24a81ac
Eduardo Muterle <eduardomuterle@outlook.com>
2021-10-25 00:36:20 -0300

    Frame Registro

---
+++ src/main/java/com/view/controllers/CRegistro.java
@@ -0,0 +1,47 @@
+package com.view.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class CRegistro {
+	
+	@FXML
+	public GridPane gridPane;
+	@FXML
+	public MenuItem menuAdicionar;
+	@FXML
+	public MenuItem menuItem;
+	@FXML
+	public MenuItem menuEditar;
+	@FXML
+	public MenuItem menuApagar;
+	@FXML
+	public TableView tablePro;
+	@FXML
+	public TableView tableAce;
+	@FXML
+	public TextField txtUsuario;
+	@FXML
+	public TextField txtNivel;
+	
+	@FXML
+	public void actMenuAdicionar() {
+		
+	}
+	@FXML
+	public void actMenuItem() {
+		
+	}
+	@FXML
+	public void actMenuEditar() {
+		
+	}
+	@FXML
+	public void actMenuApagar() {
+		
+	}
+
+}
